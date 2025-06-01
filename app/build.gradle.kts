3655ac679b1a618cb1c512fe16d2b503d991b44c
José Pérez <105478390+JoseDPerez11@users.noreply.github.com>
2025-06-01 01:33:17 -0500

    añadiendo dependencias generales

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    packaging{
+        resources {
+            excludes.add("META-INF/NOTICE.md")
+            excludes.add("META-INF/LICENSE.md")
+        }
+    }
 }
 
 dependencies {
@@ -37,6 +43,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation(libs.lifecycle)
+    implementation(libs.annotationprocesor)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp.logging)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
